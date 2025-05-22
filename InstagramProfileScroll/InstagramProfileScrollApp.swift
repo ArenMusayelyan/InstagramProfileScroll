9e0340e759907205aaef0041d7c257d00e0fa059
ArenMusayelyan <aren.musayelyan555@gmail.com>
2025-05-22 21:23:18 +0400

    Initial Commit

---
+++ InstagramProfileScroll/InstagramProfileScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramProfileScrollApp.swift
+//  InstagramProfileScroll
+//
+//  Created by Aren Musayelyan on 22.05.25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramProfileScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
